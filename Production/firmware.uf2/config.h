1c8ce0ba47f6cfbeddaca9939ea423fea7f03ceb
zhoud2019 <zhoud2019@gmail.com>
2026-03-29 18:52:48 -0400

    Organized all of the files

---
+++ Production/firmware.uf2/config.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#define VENDOR_ID    0xFEED
+#define PRODUCT_ID   0x0000
+#define DEVICE_VER   0x0001
+#define MANUFACTURER "Your Name"
+#define PRODUCT      "Macropad"
+
+#define ENCODER_RESOLUTION 4
